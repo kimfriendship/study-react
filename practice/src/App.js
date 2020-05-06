cb90733cbd94e564f247f612cab630f19a864cca
kimfriendship <kfriendship94@gmail.com>
2020-05-06 22:24:44 +0900

    :sparkles: Create another React App for practice

---
+++ practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hello from "./Hello.js";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
