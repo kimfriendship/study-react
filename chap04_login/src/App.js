e9fd4b04b7ce0db1e2f2c6c7af7d40b76ce8c26d
kimfriendship <kfriendship94@gmail.com>
2020-05-08 14:58:04 +0900

    :bug: Fix login form by using classnames

--- chap04_login/src/App.js
+++ chap04_login/src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import './App.css';
+import classNames from 'classnames/bind';
+import style from './App.css';
+
+const st = classNames.bind(style);
 
 class App extends Component {
-  loginBtn = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
     console.log('로그인 시도');
   };
 
+  handleId = () => {
+    console.log('사용자 아이디 입력중');
+  };
+
+  handlePw = () => {
+    console.log('사용자 비밀번호 입력중');
+  };
+
   render() {
     return (
-      <form>
-        <h1>로그인</h1>
-        <div>
-          <span>ID:</span>
-          <input onKeyUp={() => console.log('사용자 아이디 입력중')} />
-          <span>PW:</span>
-          <input onKeyUp={() => console.log('사용자 아이디 입력중')} />
-          <button type="submit" onClick={this.loginBtn}>
+      <form className={st('loginForm')} onSubmit={this.handleSubmit}>
+        <h1 className={st('loginTitle')}>로그인</h1>
+        <div className={st('loginWrapper')}>
+          <span className={st('loginInputs')}>ID:</span>
+          <input
+            className={st('loginInputs')}
+            type="text"
+            onChange={this.handleId}
+          />
+          <span className={st('loginInputs')}>PW:</span>
+          <input
+            className={st('loginInputs')}
+            type="password"
+            onChange={this.handlePw}
+          />
+          <button className={st('loginButton')} type="submit">
             로그인
           </button>
         </div>
