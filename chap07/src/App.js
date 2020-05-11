416c68e4d51ead27434f9e4f5e7741560efa92ca
kimfriendship <kfriendship94@gmail.com>
2020-05-11 11:24:48 +0900

    :sparkles: Make lotto

--- chap07/src/App.js
+++ chap07/src/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Child from "./Child.js";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef();
   const [lotto, setLotto] = useState([0, 0, 0, 0, 0, 0, 0]);
-  const ParentLotto = lotto;
-  const makeLotto = () => {};
+  const parentLotto = lotto;
+
+  const makeLotto = () => {
+    let lottoArray = [];
+    for (let i = 0; i <= 6; i++) {
+      let number = Math.floor(Math.random() * 45 + 1);
+      if (lottoArray.includes(number)) {
+        i -= 1;
+      } else {
+        lottoArray = [...lottoArray, number];
+      }
+    }
+    setLotto(lottoArray);
+  };
+
+  const changeInput = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
     <div className="App">
-      <Child />
+      <h2>부모 컴포넌트</h2>
+      <button onClick={makeLotto}>로또 생성기</button>
+      <input type="text" ref={inputRef} onChange={changeInput} />
+      <h1>{parentLotto.join(",")}</h1>
+      <Child lotto={parentLotto} />
     </div>
   );
 }
--- chap07/src/Child.js
+++ chap07/src/Child.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Child = () => {};
+const Child = ({ lotto }) => {
+  return (
+    <>
+      <h2>자식 컴포넌트</h2>
+      <div>
+        {lotto.map((lt, i) =>
+          i === 6 ? <span>{lt}</span> : <span>{lt + ","}</span>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Child;
