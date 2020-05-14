d97b0dd24c9c2fd3c0dcca5e5aa538fa62b64b20
kimfriendship <kfriendship94@gmail.com>
2020-05-14 17:39:27 +0900

    :sparkles: Practice using useReducer

--- chap09_useReducer/src/Book.js
+++ chap09_useReducer/src/Book.js
@@ -1,32 +1,54 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import List from "./List";
 
-const Book = () => {
-  const [info, setInfo] = useState([
-    { key: 1, name: "김우정", phone: "01080242242" },
-  ]);
-
-  const [inputs, setInputs] = useState({
+const initialState = {
+  info: [{ key: 1, name: "김우정", phone: "01080242242" }],
+  inputs: {
     name: "",
     phone: "",
-  });
+  },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUTS":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CLICK":
+      return {
+        ...state,
+        inputs: initialState.inputs,
+        info: state.info.concat(action.info),
+      };
+    default:
+  }
+};
+
+const Book = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const onChange = (e) => {
     const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
+    dispatch({
+      type: "CHANGE_INPUTS",
+      name,
+      value,
     });
   };
 
   const onClick = () => {
-    setInfo([
-      ...info,
-      { key: Math.random(), name: inputs.name, phone: inputs.phone },
-    ]);
-    setInputs({
-      name: "",
-      phone: "",
+    dispatch({
+      type: "CLICK",
+      info: {
+        key: Math.random(),
+        name: state.inputs.name,
+        phone: state.inputs.phone,
+      },
     });
   };
 
@@ -36,19 +58,19 @@ const Book = () => {
         <input
           name="name"
           type="text"
-          value={inputs.name}
+          value={state.inputs.name}
           onChange={onChange}
         />
         <input
           name="phone"
           type="text"
-          value={inputs.phone}
+          value={state.inputs.phone}
           onChange={onChange}
         />
         <button onClick={onClick}>등록</button>
       </div>
       <div>
-        <List info={info} />
+        <List info={state.info} />
       </div>
     </>
   );
