25ea72acb3d71099ef0cafc0d03e9c9848ba541e
kimfriendship <kfriendship94@gmail.com>
2020-05-19 21:30:12 +0900

    :truck: Split files

--- misaemisae/src/App.js
+++ misaemisae/src/App.js
@@ -1,59 +1,17 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import "./App.css";
+import useAsync from "./useAsync.js";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SUCCESS":
-      return {
-        misae: action.misae,
-        error: null,
-        loading: false,
-      };
-    case "ERROR":
-      return {
-        misae: null,
-        error: action.error,
-        loading: false,
-      };
-    case "LOADING":
-      return {
-        misae: null,
-        error: null,
-        loading: true,
-      };
-    default:
-      throw new Error("ERROR");
-  }
+const getMisae = async () => {
+  const response = await axios.get(
+    "http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99"
+  );
+  return response;
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {
-    misae: null,
-    error: null,
-    loading: false,
-  });
-
-  const getMisae = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get(
-        "http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99"
-      );
-      dispatch({
-        type: "SUCCESS",
-        misae: response.data.RealtimeCityAir.row,
-        error: null,
-        loading: false,
-      });
-    } catch (e) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
-
-  useEffect(() => {
-    getMisae();
-  }, []);
+  const [state, fetchMisae] = useAsync(getMisae, []);
 
   const { misae, error, loading } = state;
   if (loading) return <div>Loading...</div>;
