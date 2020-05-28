09306b172f2734748b8d9d087407ddb9b124df85
kimfriendship <kfriendship94@gmail.com>
2020-05-28 11:55:18 +0900

    :sparkles: Learn how to optimize useSelector

--- chap15_redux/src/containers/CounterContainer.js
+++ chap15_redux/src/containers/CounterContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Counter from "../components/Counter";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { increase, decrease, setDiff } from "../modules/counter";
 
 const CounterContainer = () => {
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
+
+  // const number = useSelector((state) => state.counter.number);
+  // const diff = useSelector((state) => state.counter.diff);
 
   const dispatch = useDispatch();
 
