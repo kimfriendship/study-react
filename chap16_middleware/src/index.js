6f27b9d0fb70d1fd9d55de033ac5e82dff581edc
kimfriendship <kfriendship94@gmail.com>
2020-05-28 17:23:24 +0900

    :sparkles: Try using middleware

--- chap16_middleware/src/index.js
+++ chap16_middleware/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+import myLogger from "./middlewares/myLogger";
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
