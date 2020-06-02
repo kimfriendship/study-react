668fdc8ff3ad4f041d0ce8cf74eec65c739502be
kimfriendship <kfriendship94@gmail.com>
2020-06-02 22:26:11 +0900

    :sparkles: Make counter again

---
+++ chap16_middleware/src/containers/CounterContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Counter from "../components/Counter.js";
+import { increase, decrease } from "../modules/counter.js";
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => dispatch(increase());
+  const onDecrease = () => dispatch(decrease());
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
--- chap16_middleware/src/index.js
+++ chap16_middleware/src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import App from "./App";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
 
-ReactDOM.render(document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
