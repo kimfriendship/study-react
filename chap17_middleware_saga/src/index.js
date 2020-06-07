6a038be2170635898aa564102cc95a985d5e2366
kimfriendship <kfriendship94@gmail.com>
2020-06-07 22:39:19 +0900

    :sparkles: Add router with saga

--- chap17_middleware_saga/src/index.js
+++ chap17_middleware_saga/src/index.js
@@ -10,24 +10,37 @@ import rootReducer, { rootSaga } from "./modules";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import createSagaMiddleware from "redux-saga";
 
-const sagaMiddleware = createSagaMiddleware();
+const customHistory = createBrowserHistory();
+
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history: customHistory,
+  },
+});
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk, sagaMiddleware, logger))
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware,
+      logger
+    )
+  )
 );
 
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={customHistory}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 
--- chap17_middleware_saga/src/modules/posts.js
+++ chap17_middleware_saga/src/modules/posts.js
@@ -5,7 +5,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById,
 } from "../lib/asyncUtils";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, getContext } from "redux-saga/effects";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -14,6 +14,8 @@ const GET_POSTS_ERROR = "GET_POSTS_ERROR";
 const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
+
+const GO_TO_HOME = "GO_TO_HOME";
 
 // export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 // export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
@@ -24,10 +26,18 @@ export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
 
+function* goToHomeSaga() {
+  const history = yield getContext("history");
+  history.push("/");
+}
+
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
+
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 const initialState = {
   posts: reducerUtils.initial(),
