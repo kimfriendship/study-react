5020d5c1066cdc1525c5e361754405e684de20dc
kimfriendship <kfriendship94@gmail.com>
2020-07-08 22:23:05 +0900

    :sparkles: Practice using portal

---
+++ chap18_portals/src/MyModal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MyModal.css";
+
+const MyModal = () => {
+  return (
+    <div className="MyModal">
+      <div className="content">
+        <h3>this is modal</h3>
+        <p>blabalbalbal</p>
+        <button>close</button>
+      </div>
+    </div>
+  );
+};
+
+export default MyModal;
