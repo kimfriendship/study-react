0ddacd8b1288d0f12f78ffe34c761879f9299986
kimfriendship <kfriendship94@gmail.com>
2020-07-22 20:58:17 +0900

    :heavy_plus_sign: Add styled-component

--- chap13_style/src/App.js
+++ chap13_style/src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import CheckboxWrapper from "./Components/CheckboxWrapper";
+import styled from "styled-components";
+// import CheckboxWrapper from "./Components/CheckboxWrapper";
 // import Buttons from "./Components/Buttons";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  margin: 1rem;
+  ${(props) =>
+    props.huge &&
+    `
+    width: 10rem;
+    height: 10rem;
+  `}
+`;
 
 function App() {
   return (
     <div className="App">
-      <CheckboxWrapper />
+      <Circle color="blue" />
+      <Circle color="green" huge />
     </div>
   );
 }
