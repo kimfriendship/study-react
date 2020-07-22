9ec590765381eb731e979e23dc6d56bf9457ca89
kimfriendship <kfriendship94@gmail.com>
2020-07-22 17:48:12 +0900

    :sparkles: Try styling with props

--- chap13_style/src/App.js
+++ chap13_style/src/App.js
@@ -5,7 +5,51 @@ import Button from "./Components/Button";
 function App() {
   return (
     <div className="App">
-      <Button>BUTTON</Button>
+      <div className="buttons">
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="green">
+          BUTTON
+        </Button>
+        <Button color="green">BUTTON</Button>
+        <Button size="small" color="green">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="green" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" fullWidth>
+          BUTTON
+        </Button>
+        <Button color="green" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" fullWidth>
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
--- chap13_style/src/Components/Button.js
+++ chap13_style/src/Components/Button.js
@@ -1,8 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 import "./buttons.scss";
 
-const Button = ({ children }) => {
-  return <button className="button">{children}</button>;
+// size: small, medium, large
+// color: green, blue, pink
+const Button = ({ children, size, color, outline, fullWidth }) => {
+  return (
+    <button
+      className={classNames("button", size, color, { outline, fullWidth })}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
 };
 
 export default Button;
