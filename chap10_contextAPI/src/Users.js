3d730f22809f0ea85162d825fc15809da7216cb3
kimfriendship <kfriendship94@gmail.com>
2020-07-29 20:26:36 +0900

    :sparkles: Use useReducer instead of useState

--- chap10_contextAPI/src/Users.js
+++ chap10_contextAPI/src/Users.js
@@ -1,39 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        error: null,
+        data: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        error: action.error,
+        data: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        error: null,
+        data: action.data,
+      };
+    default:
+      throw new Error(`Unhandled action type ${action.type}`);
+  }
+};
+
 const Users = () => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: null,
+    data: null,
+  });
 
   const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
-      setUsers(response.data);
+      dispatch({ type: "SUCCESS", data: response.data });
     } catch (e) {
-      console.log(e.response.data);
-      setError(e);
+      dispatch({ type: "ERROR", error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const { loading, error, data } = state;
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>ERROR!</div>;
-  if (!users) return null;
+  if (!data) return null;
 
   return (
     <>
       <ul>
-        {users.map((user) => (
+        {data.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
