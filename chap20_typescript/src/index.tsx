94c455b49a9e54d7f5d77b6c0a2a81268c0638d4
kimfriendship <kfriendship94@gmail.com>
2021-01-19 22:19:48 +0900

    :sparkles: Try using redux-saga with typescript

--- chap20_typescript/src/index.tsx
+++ chap20_typescript/src/index.tsx
@@ -5,10 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import Thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(rootReducer, applyMiddleware(Thunk));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
